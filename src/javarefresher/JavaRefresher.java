24bfe65730d823d3fda26e9152d7989243797d18
venky <venky.kumar@gmail.com>
2017-08-26 13:14:29 -0700

    Some comment change

--- src/javarefresher/JavaRefresher.java
+++ src/javarefresher/JavaRefresher.java
@@ -15,7 +15,7 @@ public class JavaRefresher {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here   dasdf
         MyObj1 obj = new MyObj1();
         obj.display();
     }
